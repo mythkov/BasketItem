222cd2c8d08286ca9fcdfbc25d582bc77a7ce9f2
Valentin Mitkov <valentin.mitkov@student.ucll.be>
2024-02-12 20:28:59 +0100

    1ste commit

---
+++ BasketItem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasketItem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
